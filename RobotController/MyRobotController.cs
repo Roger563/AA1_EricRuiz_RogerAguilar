750e2a9c7c2a84ef0db17ec000411e78d7c5043a
Eric <eric.ruiz@enti.cat>
2022-10-23 19:18:53 +0200

    Finished ex 3

--- RobotController/MyRobotController.cs
+++ RobotController/MyRobotController.cs
@@ -59,13 +59,13 @@ namespace RobotController
         public float z;
     }
 
-
-
-
-
-
     public class MyRobotController
     {
+        // EXERCICIE 2
+        private float totalLerpValue = 1.0f;
+        private float lerpValue = 0.0f;
+        private float lerpSpeed = 0.01f;
+        private float e3_lerpSpeed = 0.0025f;
 
         private float r0_initRotation = 74.0f;
         private float r0_endRotation = 40.0f;
@@ -81,10 +81,10 @@ namespace RobotController
         private float r3Twist_initRotation = 0.0f;
         private float r3Swing_initRotation = 0.0f;
         private float e3_r0_endRotation = 40.0f;
-        private float e3_r1_endRotation = -7.0f;
+        private float e3_r1_endRotation = -5.0f;
         private float e3_r2_endRotation = 100.0f;
         private float e3Twist_r3_endRotation = 90.0f;
-        private float e3Swing_r3_endRotation = 0.0f;
+        private float e3Swing_r3_endRotation = 7.0f;
 
         bool e3_finished = false;
         #region public methods
@@ -126,9 +126,9 @@ namespace RobotController
             rot3 = NullQ;
 
             rot0 = Rotate(rot0, yAxis, (float)(74.0f * (Math.PI / 180.0f)));
-            rot1 = rot0 * Rotate(rot0, xAxis, (float)(-14.0f * (Math.PI / 180.0f)));
-            rot2 = rot1 * Rotate(rot1, xAxis, (float)(116.0f * (Math.PI / 180.0f)));
-            rot3 = rot2 * Rotate(rot2, yAxis, (float)(0.0f * (Math.PI / 180.0f)));
+            rot1 = rot0 * Rotate(rot1, xAxis, (float)(-14.0f * (Math.PI / 180.0f)));
+            rot2 = rot1 * Rotate(rot2, xAxis, (float)(116.0f * (Math.PI / 180.0f)));
+            rot3 = rot2 * Rotate(rot3, yAxis, (float)(0.0f * (Math.PI / 180.0f)));
         }
 
 
@@ -139,53 +139,6 @@ namespace RobotController
 
         public bool PickStudAnim(out MyQuat rot0, out MyQuat rot1, out MyQuat rot2, out MyQuat rot3)
         {
-
-            bool myCondition = false;
-            //todo: add a check for your condition
-
-
-
-            if (myCondition)
-            {
-                //todo: add your code here
-                rot0 = NullQ;
-                rot1 = NullQ;
-                rot2 = NullQ;
-                rot3 = NullQ;
-
-
-                float newRot0 = r0_initRotation - ((r0_initRotation - r0_endRotation) * tParam);
-                float newRot1 = r1_initRotation - ((r1_initRotation - r1_endRotation) * tParam);
-                float newRot2 = r2_initRotation - ((r2_initRotation - r2_endRotation) * tParam);
-               
-
-                rot0 = Rotate(rot0, yAxis, (float)(newRot0 * (Math.PI / 180.0f)));
-                rot1 = rot0 * Rotate(rot0, xAxis, (float)(newRot1 * (Math.PI / 180.0f)));
-                rot2 = rot1 * Rotate(rot1, xAxis, (float)(newRot2 * (Math.PI / 180.0f)));
-                rot3 = rot2 * Rotate(rot2, yAxis, (float)(0.0f * (Math.PI / 180.0f)));
-
-                return true;
-            }
-
-            //todo: remove this once your code works.
-            rot0 = NullQ;
-            rot1 = NullQ;
-            rot2 = NullQ;
-            rot3 = NullQ;
-
-            return false;
-        }
-
-
-        //EX3: this function will calculate the rotations necessary to move the arm of the robot until its end effector collides with the target (called Stud_target)
-        //it will return true until it has reached its destination. The main project is set up in such a way that when the function returns false, the object will be droped and fall following gravity.
-        //the only difference wtih exercise 2 is that rot3 has a swing and a twist, where the swing will apply to joint3 and the twist to joint4
-
-        static MyQuat totalRot = NullQ;
-        
-        public bool PickStudAnimVertical(out MyQuat rot0, out MyQuat rot1, out MyQuat rot2, out MyQuat rot3)
-        {
-
             MyVec xAxis;
             xAxis.x = 1;
             xAxis.y = 0;
@@ -212,16 +165,72 @@ namespace RobotController
 
                 float tParam = lerpValue / totalLerpValue;
 
-                float newRot0 = r0_initRotation - ((r0_initRotation - e3_r0_endRotation) * tParam);
-                float newRot1 = r1_initRotation - ((r1_initRotation - e3_r1_endRotation) * tParam);
-                float newRot2 = r2_initRotation - ((r2_initRotation - e3_r2_endRotation) * tParam);
-                float newRot3Swing = r3Twist_initRotation - ((r3Twist_initRotation - e3Twist_r3_endRotation) * tParam);
-                float newRot3Twist = r3Swing_initRotation - ((r3Swing_initRotation - e3Swing_r3_endRotation) * tParam);
+                float newRot0 = r0_initRotation - ((r0_initRotation - r0_endRotation) * tParam);
+                float newRot1 = r1_initRotation - ((r1_initRotation - r1_endRotation) * tParam);
+                float newRot2 = r2_initRotation - ((r2_initRotation - r2_endRotation) * tParam);
+
 
                 rot0 = Rotate(rot0, yAxis, (float)(newRot0 * (Math.PI / 180.0f)));
                 rot1 = rot0 * Rotate(rot0, xAxis, (float)(newRot1 * (Math.PI / 180.0f)));
                 rot2 = rot1 * Rotate(rot1, xAxis, (float)(newRot2 * (Math.PI / 180.0f)));
-                rot3 = Rotate(rot2, xAxis, (float)(newRot3Swing * (Math.PI / 180.0f)))* Rotate(rot2, yAxis, (float)(newRot3Twist * (Math.PI / 180.0f)));//swing and twist rotation
+                rot3 = rot2 * Rotate(rot2, yAxis, (float)(0.0f * (Math.PI / 180.0f)));
+
+                return true;
+            }
+            else
+            {
+                lerpValue = 0.0f;
+                e2_finished = true;
+                return false;
+            }
+        }
+
+
+        //EX3: this function will calculate the rotations necessary to move the arm of the robot until its end effector collides with the target (called Stud_target)
+        //it will return true until it has reached its destination. The main project is set up in such a way that when the function returns false, the object will be droped and fall following gravity.
+        //the only difference wtih exercise 2 is that rot3 has a swing and a twist, where the swing will apply to joint3 and the twist to joint4
+
+        static MyQuat totalRot = NullQ;
+
+        public bool PickStudAnimVertical(out MyQuat rot0, out MyQuat rot1, out MyQuat rot2, out MyQuat rot3)
+        {
+
+            MyVec xAxis;
+            xAxis.x = 1;
+            xAxis.y = 0;
+            xAxis.z = 0;
+
+            MyVec yAxis;
+            yAxis.x = 0;
+            yAxis.y = 1;
+            yAxis.z = 0;
+
+            MyVec zAxis;
+            zAxis.x = 0;
+            zAxis.y = 0;
+            zAxis.z = 1;
+
+            rot0 = NullQ;
+            rot1 = NullQ;
+            rot2 = NullQ;
+            rot3 = NullQ;
+
+            if (totalLerpValue > lerpValue && !e2_finished)
+            {
+                lerpValue += e3_lerpSpeed;
+
+                float tParam = lerpValue / totalLerpValue;
+
+                float newRot0 = r0_initRotation - ((r0_initRotation - e3_r0_endRotation) * tParam);
+                float newRot1 = r1_initRotation - ((r1_initRotation - e3_r1_endRotation) * tParam);
+                float newRot2 = r2_initRotation - ((r2_initRotation - e3_r2_endRotation) * tParam);
+                float newRot3Twist = r3Twist_initRotation - ((r3Twist_initRotation - e3Twist_r3_endRotation) * tParam);
+                float newRot3Swing = r3Swing_initRotation - ((r3Swing_initRotation - e3Swing_r3_endRotation) * tParam);
+
+                rot0 = Rotate(rot0, yAxis, (float)(newRot0 * (Math.PI / 180.0f)));
+                rot1 = rot0 * Rotate(rot0, xAxis, (float)(newRot1 * (Math.PI / 180.0f)));
+                rot2 = rot1 * Rotate(rot1, xAxis, (float)(newRot2 * (Math.PI / 180.0f)));
+                rot3 = Rotate(rot2, xAxis, (float)(newRot3Swing * (Math.PI / 180.0f))) * Rotate(rot2, yAxis, (float)(newRot3Twist * (Math.PI / 180.0f)));//swing and twist rotation
 
                 totalRot = rot2;
                 return true;
@@ -238,29 +247,31 @@ namespace RobotController
         public static MyQuat GetSwing(MyQuat rot3)
         {
             //todo: change the return value for exercise 3
-            MyQuat quatToReturn = GetTwist(rot3) * totalRot.Inverse();
-            quatToReturn.x *= -1;
-            quatToReturn.y *= -1;
-            quatToReturn.z *= -1;
-            return totalRot* quatToReturn * rot3;
-            
-
+            return totalRot * (rot3 * CalculateTwist(rot3).Inverse());
         }
 
 
         public static MyQuat GetTwist(MyQuat rot3)
         {
             //todo: change the return value for exercise 3
-            MyQuat quatToReturn;
-            quatToReturn.x = 0;
-            quatToReturn.y = 0;
-            quatToReturn.z = rot3.z * (float)(1 / Math.Sqrt(Math.Pow(rot3.w, 2) + Math.Pow(rot3.z, 2)));
-            quatToReturn.w = rot3.w* (float)(1 / Math.Sqrt(Math.Pow(rot3.w, 2) + Math.Pow(rot3.z, 2)));
-            return totalRot * quatToReturn;
+            return GetSwing(rot3) * CalculateTwist(rot3);
 
+            //MyQuat quatToReturn = GetSwing(rot3);
+            //quatToReturn.x *= -1;
+            //quatToReturn.y *= -1;
+            //quatToReturn.z *= -1;
+            //return quatToReturn * totalRot;
         }
 
-
+        private static MyQuat CalculateTwist(MyQuat rot)
+        {
+            MyQuat qt;
+            qt.x = 0;
+            qt.y = rot.y;
+            qt.z = 0;
+            qt.w = rot.w;
+            return qt.Normalize();
+        }
 
 
         #endregion
@@ -285,7 +296,8 @@ namespace RobotController
             }
         }
 
-        internal MyQuat Multiply(MyQuat q1, MyQuat q2) {
+        internal MyQuat Multiply(MyQuat q1, MyQuat q2)
+        {
 
             //todo: change this so it returns a multiplication:
             return NullQ;
